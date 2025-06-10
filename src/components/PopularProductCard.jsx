e0ea51b21970ba9b1d76b5c31c79e0f5b6734e9b
Mikhil812 <mikhilangre812@gmail.com>
2025-06-10 01:35:41 -0500

    Completed the Popular Products section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { star } from '../assets/icons'
 
 const PopularProductCard = ({imgURL, name, price}) => {
   return (
-    <div className='flex flex-1 flex-col'>
+    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
         <img 
             src={imgURL}
-            alt='Popular Product'
+            alt={name}
             width={280}
             height={280}
         />
-        <h3>{name}</h3>
+        <div className='mt-8 flex justify-start gap-2.5 '>
+          <img 
+            src={star}
+            alt='rating'
+            width={24}
+            height={24}
+          />
+          <p className='text-xl font-montserrat text-slate-gray leading-normal'>(4.5)</p>
+        </div>
+        <h3 className='text-2xl font-palanquin font-semibold leading-normal mt-1'>{name}</h3>
+        <p className='font-montserrat leading-normal text-coral-red font-medium'>{price}</p>
     </div>
   )
 }
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -4,12 +4,12 @@ import PopularProductCard from '../components/PopularProductCard.jsx'
 
 const PopularProducts = () => {
   return (
-    <section id="products" className='bg-gray-400 max-sm:mt-12'>
-      <div className='mt-20 bg-yellow-100 flex flex-col justify-start gap-5'>
+    <section id="products" className=' max-sm:mt-12'>
+      <div className='flex flex-col justify-start gap-5'>
         <h2 className='text-4xl font-palanquin'>Our <span className=' text-coral-red'>Popular</span> Products</h2>
         <p className='mt-5 font-montserrat text-slate-gray'>Our product blah blah blah just trying to do this popular products section by myself. Blah.</p>
       </div>
-      <div className='mt-14 bg-green-100 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14'>
+      <div className='mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14'>
       {products.map((product) => (
           <PopularProductCard key={product.name} {...product}/>
         ))}
